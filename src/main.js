cfadb077973d4d1c28f512fb9899b6ccfa7aee29
Y5563 <script.robot.me@gmail.com>
2022-06-23 07:48:28 +0100

    This is my first commit

---
+++ src/main.js
@@ -0,0 +1,226 @@
+let count = 0;
+
+
+let funCart = document.querySelector('#cart');
+let funCart2 = document.querySelector('#cart2');
+let funCart3 = document.querySelector('#cart3');
+let funCart4 = document.querySelector('#cart4');
+let funCart5 = document.querySelector('#cart5');
+let funCart6 = document.querySelector('#cart6');
+let funCart7 = document.querySelector('#cart7');
+let funCart8 = document.querySelector('#cart8');
+let funCart9 = document.querySelector('#cart9');
+let funCart10 = document.querySelector('#cart10');
+let funCart11 = document.querySelector('#cart11');
+let funCart12 = document.querySelector('#cart12');
+let funCart13= document.querySelector('#cart13');
+let funCart14 = document.querySelector('#cart14');
+let funCart15 = document.querySelector('#cart15');
+let funCart16 = document.querySelector('#cart16');
+
+const cart = () => {
+  funCart.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart();
+
+// 2
+const cart2 = () => {
+  funCart2.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart2();
+
+// 3
+const cart3 = () => {
+  funCart3.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart3();
+
+// 4
+const cart4 = () => {
+  funCart4.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart4();
+
+// 5
+const cart5 = () => {
+  funCart5.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart5();
+
+ // 6
+const cart6 = () => {
+  funCart6.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart6();
+
+// 7
+const cart7 = () => {
+  funCart7.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart7();
+
+// 8
+const cart8 = () => {
+  funCart8.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart8();
+
+// 9
+const cart9 = () => {
+  funCart9.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart9();
+
+// 10
+const cart10 = () => {
+  funCart10.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart10();
+
+// 11
+const cart11 = () => {
+  funCart11.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart11();
+
+// 12
+const cart12 = () => {
+  funCart12.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart12();
+
+// 13
+const cart13 = () => {
+  funCart13.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart13();
+
+// 14
+const cart14 = () => {
+  funCart14.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart14();
+
+// 15
+const cart15 = () => {
+  funCart15.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart15();
+
+// 16
+const cart16 = () => {
+  funCart16.addEventListener(
+    'click',
+    (e) => {
+      document.querySelector('#cart-number').innerText = count += 1;
+      alert(`Added to cart successfuly`);
+    },
+    { once: true }
+  );
+};
+cart16();
---
+++ src/script.js
@@ -0,0 +1,189 @@
+// let count = 0;
+// let count2 = 0;
+// let count3 = 0;
+// let count4 = 0;
+// let shirt = 0;
+
+// const plusCount = () => {
+//   document.querySelector('#plus').addEventListener('click', e=>{
+//     const number = document.querySelector('#numberCount').innerText = count += 1;
+//     document.querySelector('#cart-price').innerText = shirt  += 100;
+//     if(number === 10) alert('The maximum is 10 item')
+
+//   })
+// }
+// plusCount()
+
+// const minusCount = () => {
+//   document.querySelector('#minus').addEventListener('click', e=>{
+//     document.querySelector('#numberCount').innerText = count -= 1;
+//     document.querySelector('#cart-price').innerText = shirt -= 100;
+//     if(count === 1){
+//       alert('The minimum you can order is 1')
+
+//     }
+//   })
+// }
+
+// minusCount()
+
+// const plusCount2 = () => {
+//    document.querySelector('#plus2').addEventListener('click', (e) => {
+//    document.querySelector('#numberCount2').innerText = count2 += 1;
+//    document.querySelector('#cart-number').innerText = count2;
+//   });
+// };
+// plusCount2();
+
+// const minusCount2 = () => {
+//   document.querySelector('#minus2').addEventListener('click', (e) => {
+//     document.querySelector('#numberCount2').innerText = count2 -= 1;
+//     document.querySelector('#cart-number').innerText = count2;
+//   });
+// };
+
+// minusCount2();
+
+// const plusCount3 = () => {
+//   document.querySelector('#plus3').addEventListener('click', (e) => {
+//     document.querySelector('#numberCount3').innerText = count3 += 1;
+//     document.querySelector('#cart-number').innerText = count3;
+//   });
+// };
+// plusCount3();
+
+// const minusCount3 = () => {
+//   document.querySelector('#minus3').addEventListener('click', (e) => {
+//     document.querySelector('#numberCount3').innerText = count3 -= 1;
+//     document.querySelector('#cart-number').innerText = count3;
+//   });
+// };
+
+// minusCount3();
+
+// const plusCount4 = () => {
+//   document.querySelector('#plus4').addEventListener('click', (e) => {
+//     document.querySelector('#numberCount4').innerText = count4 += 1;
+//     document.querySelector('#cart-number').innerText = count4;
+//   });
+// };
+// plusCount4();
+
+// const minusCount4 = () => {
+//   document.querySelector('#minus4').addEventListener('click', (e) => {
+//     document.querySelector('#numberCount4').innerText = count4 -= 1;
+//     document.querySelector('#cart-number').innerText = count4;
+//   });
+// };
+
+// minusCount4();
+
+
+const card = document.querySelector('#shop');
+
+
+const eventCart = () => {
+  return (card.innerHTML = `
+     <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="./image/img-1.jpg" class="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Casual Shirt</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional.</p>
+                    </div>
+                    <div class="amount">
+                        <h6>$<h6 id="cart-price">100</h6></h6>
+                        <div class="zero">
+                         <a href="buying.html" <i class="bi bi-bag"></i></a>
+                            <i class="bi bi-trash3 "></i>
+                        </div>
+                        <div class="zero">
+                            <i class="bi bi-dash" id="minus"></i>
+                            <div class="quantity" id="numberCount">1</div>
+                              <i class="bi bi-plus" id="plus"></i>
+                         </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="./image/img-2.jpg" class="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Casual Shirt</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional.</p>
+                    </div>
+                    <div class="amount">
+                        <h6>$<h6 id="cart-price">100</h6></h6>
+                        <div class="zero">
+                         <a href="buying.html" <i class="bi bi-bag"></i></a>
+                            <i class="bi bi-trash3 "></i>
+                        </div>
+                        <div class="zero">
+                            <i class="bi bi-dash" id="minus"></i>
+                            <div class="quantity" id="numberCount">1</div>
+                              <i class="bi bi-plus" id="plus"></i>
+                         </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="./image/img-3.jpg" class="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Casual Shirt</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional.</p>
+                    </div>
+                    <div class="amount">
+                        <h6>$<h6 id="cart-price">100</h6></h6>
+                        <div class="zero">
+                         <a href="buying.html" <i class="bi bi-bag"></i></a>
+                            <i class="bi bi-trash3 "></i>
+                        </div>
+                        <div class="zero">
+                            <i class="bi bi-dash" id="minus"></i>
+                            <div class="quantity" id="numberCount">1</div>
+                              <i class="bi bi-plus" id="plus"></i>
+                         </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card mb-3" style="max-width: 300px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="./image/img-4.jpg" class="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Casual Shirt</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional.</p>
+                    </div>
+                    <div class="amount">
+                        <h6>$<h6 id="cart-price">100</h6></h6>
+                        <div class="zero">
+                         <a href="buying.html" <i class="bi bi-bag"></i></a>
+                            <i class="bi bi-trash3 "></i>
+                        </div>
+                        <div class="zero">
+                            <i class="bi bi-dash" id="minus"></i>
+                            <div class="quantity" id="numberCount">1</div>
+                              <i class="bi bi-plus" id="plus"></i>
+                         </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
+};
+eventCart();
